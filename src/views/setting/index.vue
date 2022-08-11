b20ac70f9123a1606cef497748e55fa5086069fd
YGWW <1540612235@qq.com>
2022-08-11 09:08:37 +0800

    完成公司设置用户管理tab切换

--- src/views/setting/index.vue
+++ src/views/setting/index.vue
@@ -1,20 +1,60 @@
 <template>
   <div class="dashboard-container">
-    <div class="app-container">
-      <h2>公司设置</h2>
-    </div>
+    <template>
+      <el-tabs v-model="activeName" @tab-click="handleClick">
+        <el-tab-pane label="用户管理" name="first">
+          <el-button type="primary">新增角色</el-button></el-tab-pane
+        >
+        <el-table :data="tableData" style="width: 100%">
+          <el-table-column prop="date" label="日期" width="180">
+          </el-table-column>
+          <el-table-column prop="name" label="姓名" width="180">
+          </el-table-column>
+          <el-table-column prop="address" label="地址"> </el-table-column>
+        </el-table>
+        <el-tab-pane label="角色管理" name="third">公司信息</el-tab-pane>
+      </el-tabs>
+    </template>
   </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {}
+    return {
+      activeName: 'second',
+      tableData: [
+        {
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄',
+        },
+        {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄',
+        },
+        {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄',
+        },
+        {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄',
+        },
+      ],
+    }
   },
 
   created() {},
 
-  methods: {}
+  methods: {
+    handleClick(tab, event) {
+      console.log(tab, event)
+    },
+  },
 }
 </script>
 
