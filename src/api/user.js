e06f55919dfd77438fe00d24776a07e2425b3719
YGWW <1540612235@qq.com>
2022-08-14 16:41:57 +0800

    完成个人详情的渲染

--- src/api/user.js
+++ src/api/user.js
@@ -41,3 +41,23 @@ export function logout() {
     url: `/sys/user/${id}`
   })
 }
+// /** *
+//  *  获取某个用户的基本信息
+//  *
+//  * ***/
+//  export function getUserDetailById(id) {
+//   return request({
+//     url: `/sys/user/${id}`
+//   })
+// }
+/** *
+ *
+ * 保存员工的基本信息
+ * **/
+ export function saveUserDetailById(data) {
+  return request({
+    url: `/sys/user/${data.id}`,
+    method: 'put',
+    data
+  })
+}
--- src/views/employees/index.vue
+++ src/views/employees/index.vue
@@ -10,7 +10,9 @@
             type="warning"
             >导入</el-button
           >
-          <el-button size="small" type="danger">导出</el-button>
+          <el-button size="small" type="danger" @click="exportExcel"
+            >导出</el-button
+          >
           <el-button size="small" type="primary" @click="addStaff"
             >新增员工</el-button
           >
@@ -60,7 +62,12 @@
           </el-table-column>
           <el-table-column label="操作" sortable="" fixed="right" width="280">
             <template slot-scope="{ row }">
-              <el-button type="text" size="small">查看</el-button>
+              <el-button
+                type="text"
+                size="small"
+                @click="$router.push('/employees/detail/' + row.id)"
+                >查看</el-button
+              >
               <el-button type="text" size="small">转正</el-button>
               <el-button type="text" size="small">调岗</el-button>
               <el-button type="text" size="small">离职</el-button>
@@ -122,6 +129,17 @@ export default {
   },
 
   methods: {
+    async exportExcel() {
+      await import('@/vendor/Export2Excel').then((excel) => {
+        excel.export_json_to_excel({
+          header: ['姓名', '手机号'], //表头 必填
+          data: [['宇哥', '666666']], //具体数据 必填
+          filename: '员工列表', //非必填
+          autoWidth: true, //非必填
+          bookType: 'xlsx', //非必填
+        })
+      })
+    },
     addStaff() {
       this.addvisible = true
     },
